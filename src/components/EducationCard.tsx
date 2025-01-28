14cbcbdca265e499c120d6c2640eb7e8ad3a76d8
StrigZ <mirgasan200@gmail.com>
2025-01-28 14:36:10 +0300

    Add EducationCard component

---
+++ src/components/EducationCard.tsx
@@ -0,0 +1,33 @@
+import { GiGraduateCap } from "react-icons/gi";
+import Card from "./Card";
+import Input from "./Input";
+
+type Props = {
+  isCollapsed: boolean;
+  onCollapse: () => void;
+};
+function EducationCard({ isCollapsed, onCollapse }: Props) {
+  return (
+    <Card
+      title="Education"
+      titleIcon={<GiGraduateCap />}
+      isCollapsable
+      isCollapsed={isCollapsed}
+      onCollapse={onCollapse}
+    >
+      <Input
+        labelText="School"
+        type="text"
+        placeholder="Academy of the Sacred Heart"
+      />
+      <Input labelText="Degree" type="text" placeholder="PhD" />
+      <Input labelText="End Date" type="date" placeholder="1234567890" />
+      <Input
+        labelText="Location"
+        type="text"
+        placeholder="351 Elizabeth Avenue, Louisiana"
+      />
+    </Card>
+  );
+}
+export default EducationCard;
