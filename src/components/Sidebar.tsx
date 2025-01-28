95ad0aad786ac2b997de35e8a05055f419dc1b9f
StrigZ <mirgasan200@gmail.com>
2025-01-28 23:08:21 +0300

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,22 +3,27 @@
 import { useState } from "react";
 import EducationCard from "./EducationCard";
 import PersonalDetailsCard from "./PersonalDetails";
+import ExperienceCard from "./ExperienceCard";
+
+type CardName = "education" | "experience" | null;
 
 type Props = {};
 function Sidebar({}: Props) {
-  const [visibleCardIndex, setVisibleCardIndex] = useState<
-    "education" | "experience" | null
-  >(null);
+  const [visibleCardName, setVisibleCardName] = useState<CardName>(null);
+
+  const handleCardCollapse = (cardName: CardName) =>
+    setVisibleCardName((prev) => (prev === cardName ? null : cardName));
+
   return (
     <aside className="col-start-1 col-end-2 flex flex-col gap-4">
       <PersonalDetailsCard />
       <EducationCard
-        isCollapsed={visibleCardIndex === "education"}
-        onCollapse={() =>
-          setVisibleCardIndex((prev) =>
-            prev === "education" ? null : "education",
-          )
-        }
+        isCollapsed={visibleCardName !== "education"}
+        onCollapse={() => handleCardCollapse("education")}
+      />
+      <ExperienceCard
+        isCollapsed={visibleCardName !== "experience"}
+        onCollapse={() => handleCardCollapse("experience")}
       />
     </aside>
   );
